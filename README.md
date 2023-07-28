2a85ccb7da71262e63ff0310b4578922e70ae78f
Ioannis Christodoulakis <ioannis.ch@outlook.com>
2023-07-28 19:15:36 +0200

    update the fibonacci sequence

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# fibonacci-sequence
+# Fibonacci Sequence
+
+## Fibonacci Series up to n terms
+
+Write a program in C to display the first n terms of the Fibonacci series.
+
+## Fibonacci Sequence Up to a Certain Number
+
+Write a program in C to print all the Fibonacci numbers up to n.
+
+![Fibonacci Sequence](fibonacci_sequence)
---
+++ fibonacci_sequence_2.c
@@ -0,0 +1,28 @@
+/*
+Filename: fibonacci_sequence_2.c
+Author: Ioannis Christodoulakis
+Creation Date: 27.07.2023
+*/
+
+#include <stdio.h>
+
+int main() {
+	int a=0, b=1, c=0, n;
+	
+	printf("Enter a positive number: ");
+	scanf("%d",&n);
+	
+	printf("Fibonacci Series: %d,%d",a,b);
+	
+	c=a+b;
+	
+	while (c<=n)
+	{
+		printf(",%d",c);
+		a=b;
+		b=c;
+		c=a+b;
+	}
+	
+	return 0;
+}
